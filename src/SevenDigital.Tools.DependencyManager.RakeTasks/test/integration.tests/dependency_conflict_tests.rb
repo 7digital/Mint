9d29746272ddab6e5e8a665c7bab796770ef9539
Ben Biddington <ben.biddington@gmail.com>
2010-01-18 09:01:34 +0000

    Source formatting

--- src/SevenDigital.Tools.DependencyManager.RakeTasks/test/integration.tests/dependency_conflict_tests.rb
+++ src/SevenDigital.Tools.DependencyManager.RakeTasks/test/integration.tests/dependency_conflict_tests.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../../src/lib/chubby_rain'
 
 class DependencyConflictTests < Test::Unit::TestCase
 	
-  def test_that_a_conflict_is_returned
+	def test_that_a_conflict_is_returned
 		instance = ChubbyRain.new(EXE_PATH, WORKING_DIR)
 
 		conflict = instance.run(COMMAND_REPORT, ASS_NAME)
@@ -14,31 +14,32 @@ class DependencyConflictTests < Test::Unit::TestCase
 		assert_contains(conflict.text, "References: 			SevenDigital.B")
 		assert_contains(conflict.text, "Reference Version: 		1.0.0.0")
 		assert_contains(conflict.text, "Actual Reference Version: 	1.5.0.0")
-  end
+	end
 
- def test_that_there_are_no_conflicts
+	def test_that_there_are_no_conflicts
 		instance = ChubbyRain.new(EXE_PATH, WORKING_DIR)
 
 		conflict = instance.run(COMMAND_REPORT, NO_CONFLICT_NAME)
 
 		assert_equal(0, conflict.exit_status, "Expected exit status of 0. Message: #{conflict.text}")
 		assert_not_contains("is referencing assembly",conflict.text)
-end
-private
+	end
 
-def assert_contains(text, what)
+	private
+
+	def assert_contains(text, what)
 		assert(
 			text.include?(what),
 			"Expected text <#{what}> not found in: <#{text}>"
 		)
-end
+	end
 
-def assert_not_contains(text,what)
-	  assert(
-		 false ==  text.include?(what),
-		  "Found <#{what}> in: <#{text}>"
-	  )
-end
+	def assert_not_contains(text,what)
+		assert(
+			 false ==  text.include?(what),
+			  "Found <#{what}> in: <#{text}>"
+		)
+	end
 
 	ANY_PATH_THAT_DOES_NOT_EXIST = 'c:\does-not-exist'
 	EXE_PATH            = File.expand_path(File.dirname(__FILE__) + '/../../src/bin/chubbyrain.exe')
