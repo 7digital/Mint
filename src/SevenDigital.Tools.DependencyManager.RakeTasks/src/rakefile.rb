3853a647ccd96c25ae0d9db220a0b848e3c24b47
Ben Biddington <ben.biddington@gmail.com>
2010-05-06 16:17:39 +0100

    Fixed error in merge_with_defaults

--- src/SevenDigital.Tools.DependencyManager.RakeTasks/src/rakefile.rb
+++ src/SevenDigital.Tools.DependencyManager.RakeTasks/src/rakefile.rb
@@ -4,4 +4,4 @@ require 'lib/dependency_report_tasks'
 
 WORKING_DIR = File.dirname(__FILE__) + '/../bin/1.2.121'
 
-DependencyReportTasks.new(WORKING_DIR)
+DependencyReportTasks.new(:working_directory => WORKING_DIR)
